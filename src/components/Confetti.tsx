f849937c9316103735dcc5292781f8981c2ec12b
i3ima <hueputalo.rubin@gmail.com>
2021-08-15 20:42:22 +0300

    feat(components): update confetti on resize

--- src/components/Confetti.tsx
+++ src/components/Confetti.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { useWindowSize } from 'react-use';
 
 interface ConfettiElement {
  character: string
@@ -9,36 +10,42 @@ interface ConfettiElement {
 
 const characters = ['🚧', '🏗', '⚛', '🦀'];
 
+const createConfetti = (characters: string[], previous?: ConfettiElement[]): ConfettiElement[] => Array.from({ length: 200 }).map((_, index) => ({
+ character: characters[index % characters.length],
+ x: Math.random() * window.screen.width - 30,
+ y: Math.random() * window.screen.height,
+ r: previous ? previous[index].r : 0.5 + Math.random() * 1.5
+}))
+
 const Confetti: React.FC = () => {
-	const [confetti, setConfetti] = useState<ConfettiElement[]>(Array.from({length: 200})
-			.map((_, index) => ({
-			 character: characters[index % characters.length],
-			 x: Math.random() * window.screen.width - 30,
-			 y: Math.random() * 800,
-			 r: 0.5 + Math.random() * 1.5
-			}))
-			.sort((a, b) => a.r - b.r))
+ const { width, height } = useWindowSize();
+ const [confetti, setConfetti] = useState<ConfettiElement[]>(createConfetti(characters))
 
  const redraw = () => {
 	// eslint-disable-next-line no-return-assign
 	setConfetti(previousState => previousState.map((emoji): ConfettiElement => ({
-		...emoji,
-		y: emoji.y >= window.screen.height ? -20 : emoji.y + 0.7 * emoji.r,
-	 })))
+	 ...emoji,
+	 y: emoji.y >= window.screen.height ? -20 : emoji.y + 0.7 * emoji.r,
+	})))
  }
 
  useEffect(() => {
 
-  const frame = requestAnimationFrame(redraw)
+	const frame = requestAnimationFrame(redraw)
 
 	return () => cancelAnimationFrame(frame)
  }, [confetti])
 
+ useEffect(() => {
+	console.log('rebuilding')
+	setConfetti(previousState => createConfetti(characters, previousState))
+ }, [width, height])
+
  return (
 		 <div className="absolute top-0 left-0 w-screen h-screen overflow-hidden opacity-90">
-			{confetti.map((c) => (
-					// eslint-disable-next-line react/no-array-index-key
-					<span className="select-none absolute text-lg" style={{ left: c.x, top: c.y, transform: `scale(${c.r})` }}>{c.character}</span>
+			{confetti.map((c, index) => (
+					<span key={index + 1} className="select-none absolute text-lg"
+								style={{ left: c.x, top: c.y, transform: `scale(${c.r})` }}>{c.character}</span>
 			))}
 		 </div>
  );
