60925e897441c8ae9c0337fe463044900660da29
i3ima <hueputalo.rubin@gmail.com>
2021-08-15 20:41:28 +0300

    feat(types): add return type for useWindowSize

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,12 @@ export interface LinkType {
  url?: string
  Icon?: ReactElement
 }
+
+declare module 'react-use' {
+ const useWindowSize: (initialWidth?: number, initialHeight?: number) => WindowSize
+}
+
+interface WindowSize {
+ width: number,
+ height: number
+}
