9e49357e0ae3d4ac6676f9752f25cb2719d7d02f
i3ima <hueputalo.rubin@gmail.com>
2021-08-16 11:25:49 +0300

    fix: dumb firefox doing dumb things

--- src/components/FillBoxBackdrop.tsx
+++ src/components/FillBoxBackdrop.tsx
@@ -6,9 +6,9 @@ interface IProperties  {
 }
 
 const FillBoxBackdrop: React.FC<IProperties> = ({ children, shouldBlur = true }) => (
-		<div className={`w-11/12 md:w-8/12 lg:w-6/12 h-auto z-10 shadow-2xl rounded-2xl ${shouldBlur ? 'backdrop-blur' : ''}`}>
+		<div className={`relative w-11/12 md:w-8/12 lg:w-6/12 h-auto z-10 shadow-2xl rounded-2xl ${shouldBlur ? 'backdrop-blur' : ''}`}>
 			{/* backdrop tint */}
-		 <div className="absolute top-0 w-full h-full bg-gray-700 rounded-2xl opacity-30" />
+		 <div className="absolute  w-full h-full bg-gray-700 rounded-2xl opacity-30" />
 		 {children}
 </div>
 )
